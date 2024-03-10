3dffeca3c4456a9c430bc9fd4dd5bc82dd106e26
TaixMiguel <taixmiguel@none.com>
2024-03-10 16:13:17 +0100

    [FEATURE] User secrets creation, editing, and deletion
    The user secrets entity is defined, along with its service and
    repository classes, to enable the creation, modification, and deletion
    of secrets in the database.

---
+++ PenKaTurCore/src/main/java/es/taixmiguel/penkatur/core/profiles/user/security/UserSecretsService.java
@@ -0,0 +1,39 @@
+package es.taixmiguel.penkatur.core.profiles.user.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import es.taixmiguel.penkatur.core.profiles.user.model.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Service
+public class UserSecretsService {
+
+	@Value("${penkatur.profiles.user.security.monthsPasswordExpiration}")
+	private int monthsExpiration;
+
+	private UserSecretsRepository secretsRepo;
+
+	UserSecrets createSecrets(@NotNull User user, @NotBlank String password) {
+		return secretsRepo.save(new UserSecrets(user, password, monthsExpiration));
+	}
+
+	UserSecrets updatePassword(@NotNull UserSecrets secrets, @NotBlank String password) {
+		secrets.setPassword(password, monthsExpiration);
+		return secretsRepo.saveAndFlush(secrets);
+	}
+
+	public void deleteSecrets(User user) {
+		Optional<UserSecrets> secrets = secretsRepo.findByUser(user);
+		secrets.ifPresent(s -> secretsRepo.delete(s));
+	}
+
+	@Autowired
+	public void setUserSecretsRepository(UserSecretsRepository secretsRepo) {
+		this.secretsRepo = secretsRepo;
+	}
+}
---
+++ PenKaTurCore/src/test/java/es/taixmiguel/penkatur/core/profiles/user/security/SecurityUserTests.java
@@ -0,0 +1,107 @@
+package es.taixmiguel.penkatur.core.profiles.user.security;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import es.taixmiguel.penkatur.core.PenKaTurCoreApplication;
+import es.taixmiguel.penkatur.core.exceptions.DuplicatedUserException;
+import es.taixmiguel.penkatur.core.profiles.user.ToolUser;
+import es.taixmiguel.penkatur.core.profiles.user.model.User;
+import es.taixmiguel.penkatur.core.profiles.user.service.UserService;
+import es.taixmiguel.penkatur.core.tools.log.Log;
+
+@SpringBootTest(classes = PenKaTurCoreApplication.class)
+class SecurityUserTests {
+
+	public final String PASSWORD1 = "passW0RD";
+	public final String PASSWORD2 = "P4SSword";
+
+	@Value("${penkatur.profiles.user.security.monthsPasswordExpiration}")
+	private int monthsExpiration;
+
+	private UserSecretsService secretsService;
+	private UserService userService;
+
+	@Autowired
+	public SecurityUserTests(UserService userService, UserSecretsService secretsService) {
+		this.secretsService = secretsService;
+		this.userService = userService;
+	}
+
+	@AfterEach
+	void deleteUser() {
+		Log.trace(getClass(), "Running user cleanup");
+		userService.findUser(ToolUser.EMAIL).ifPresent(u -> userService.deleteUser(u));
+	}
+
+	@Test
+	void createSecrets0() {
+		Log.trace(getClass(), "Running test createSecrets0()");
+		UserSecrets secrets = getInstanceSecrets(PASSWORD1, 0);
+		checkSecrets(secrets, PASSWORD1, 0);
+	}
+
+	@Test
+	void changeSecrets0() {
+		Log.trace(getClass(), "Running test changeSecrets0()");
+		UserSecrets secrets = getInstanceSecrets(PASSWORD1, 0);
+		secrets.setPassword(PASSWORD2, 0);
+		checkSecrets(secrets, PASSWORD2, 0);
+	}
+
+	@Test
+	void createSecrets8() {
+		Log.trace(getClass(), "Running test createSecrets8()");
+		UserSecrets secrets = getInstanceSecrets(PASSWORD1, 8);
+		checkSecrets(secrets, PASSWORD1, 8);
+	}
+
+	@Test
+	void changeSecrets11() {
+		Log.trace(getClass(), "Running test changeSecrets11()");
+		UserSecrets secrets = getInstanceSecrets(PASSWORD1, 8);
+		secrets.setPassword(PASSWORD2, 11);
+		checkSecrets(secrets, PASSWORD2, 11);
+	}
+
+	@Test
+	void saveSecrets() throws DuplicatedUserException {
+		Log.trace(getClass(), "Running test saveSecrets()");
+		User user = getCreatedUser();
+		UserSecrets secrets = secretsService.createSecrets(user, PASSWORD1);
+		checkSecrets(secrets, PASSWORD1, monthsExpiration);
+	}
+
+	@Test
+	void updateSecrets() throws DuplicatedUserException {
+		Log.trace(getClass(), "Running test updateSecrets()");
+		User user = getCreatedUser();
+		UserSecrets secrets = secretsService.createSecrets(user, PASSWORD1);
+		secretsService.updatePassword(secrets, PASSWORD2);
+		checkSecrets(secrets, PASSWORD2, monthsExpiration);
+	}
+
+	private User getCreatedUser() throws DuplicatedUserException {
+		return userService.createUser(ToolUser.getInstanceSimpleUser());
+	}
+
+	private UserSecrets getInstanceSecrets(String password, int monthsExpiration) {
+		User user = ToolUser.getInstanceSimpleUser();
+		return new UserSecrets(user, password, monthsExpiration);
+	}
+
+	private void checkSecrets(UserSecrets secrets, String password, int monthsExpiration) {
+		assertNotNull(secrets, "The secrets instance cannot be null.");
+		assertEquals(password, secrets.getPassword(), "The password does not match the expected one.");
+		assertEquals(monthsExpiration <= 0 ? LocalDate.MAX : LocalDate.now().plusMonths(monthsExpiration),
+				secrets.getPasswordExpiration(), "The password duration does not match the calculated one.");
+	}
+}
