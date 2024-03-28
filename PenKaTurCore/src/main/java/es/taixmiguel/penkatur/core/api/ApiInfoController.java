2f45cb837e18d9de9dcc3027e92fc0a065b53368
TaixMiguel <taixmiguel@none.com>
2024-03-28 15:10:32 +0100

    [FEATURE] Info controller created

---
+++ PenKaTurCore/src/main/java/es/taixmiguel/penkatur/core/api/ApiInfoController.java
@@ -0,0 +1,28 @@
+package es.taixmiguel.penkatur.core.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "Information")
+public class ApiInfoController {
+
+	@Value("${penkatur.app.version}")
+	private String penkaturVersion;
+	@Value("${penkatur.auth.signup.enabled}")
+	private boolean signupEnabled;
+
+	@GetMapping("/info")
+	public ResponseEntity<ApiInfoResponse> showInformation() {
+		ApiInfoResponse infoResponse = new ApiInfoResponse();
+		infoResponse.setPenkaturVersion(penkaturVersion);
+		infoResponse.setSignupEnabled(signupEnabled);
+		return ResponseEntity.ok(infoResponse);
+	}
+}
