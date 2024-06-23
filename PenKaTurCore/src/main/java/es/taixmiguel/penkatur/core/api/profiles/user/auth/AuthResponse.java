7b42b1c2daa651742b212f1e3a794f2521d269c6
TaixMiguel <taixmiguel@none.com>
2024-06-23 17:19:06 +0200

    [REMOVE] Remove old attributes

--- PenKaTurCore/src/main/java/es/taixmiguel/penkatur/core/api/profiles/user/auth/AuthResponse.java
+++ PenKaTurCore/src/main/java/es/taixmiguel/penkatur/core/api/profiles/user/auth/AuthResponse.java
@@ -1,14 +1,8 @@
 package es.taixmiguel.penkatur.core.api.profiles.user.auth;
 
-import java.time.Instant;
-
-public record AuthResponse(long idUser, String message, String token, String refreshToken, Instant timestamp) {
-
-	public AuthResponse(long idUser, String message, String token, String refreshToken) {
-		this(idUser, message, token, refreshToken, Instant.now());
-	}
+public record AuthResponse(long idUser, String message, String token, String refreshToken) {
 
 	public AuthResponse(String message) {
-		this(0, message, "", "", Instant.now());
+		this(0, message, "", "");
 	}
 }
