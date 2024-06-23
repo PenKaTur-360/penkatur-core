ea8ba8c5ccb1e1ee2246babe07ea398de13c984b
TaixMiguel <taixmiguel@none.com>
2024-06-23 12:32:46 +0200

    [CHANGED] Change LocalDateTime to Instant

--- PenKaTurCore/src/main/java/es/taixmiguel/penkatur/core/api/MessageResponse.java
+++ PenKaTurCore/src/main/java/es/taixmiguel/penkatur/core/api/MessageResponse.java
@@ -1,14 +1,10 @@
 package es.taixmiguel.penkatur.core.api;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-
-public record MessageResponse(String message,
-		@JsonFormat(shape = Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss") LocalDateTime timestamp) {
+public record MessageResponse(String message, Instant timestamp) {
 
 	public MessageResponse(String message) {
-		this(message, LocalDateTime.now());
+		this(message, Instant.now());
 	}
 }
